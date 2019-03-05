7fd837a9b05f1733eacd3e635a5244ecd9567c2f
martypdx <marty@martypdx.com>
2019-03-05 14:47:17 -0800

    working search updating hash query

--- src/search-component.js
+++ src/search-component.js
@@ -5,7 +5,7 @@ const searchTermInput = searchForm.querySelector('input');
 searchForm.addEventListener('submit', event => {
     event.preventDefault();
     const searchTerm = searchTermInput.value;
-    const existingQuery = window.location.hash.slice(1);
+    const existingQuery = window.location.hash.slice(1); // get rid of leading "#"
     const newQuery = writeSearchToQuery(existingQuery, searchTerm);
     window.location.hash = newQuery;
 });
