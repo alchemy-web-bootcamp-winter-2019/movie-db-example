1f7b0c85189bbfcb00a2ea98d9a8d32b0d842b90
martypdx <marty@martypdx.com>
2019-03-05 08:36:20 -0800

    reset for demo

--- src/paging-component.js
+++ src/paging-component.js
@@ -3,28 +3,3 @@ const previousButton = document.getElementById('previous-button');
 const nextButton = document.getElementById('next-button');
 const currentPage = document.getElementById('current-page');
 const totalPages = document.getElementById('total-pages');
-
-let currentPageNumber = 1;
-
-export function updatePagingInfo(pagingInfo) {
-    pagingSection.classList.remove('hidden');
-    currentPageNumber = pagingInfo.page;
-    currentPage.textContent = currentPageNumber;
-    totalPages.textContent = pagingInfo.totalPages;
-    nextButton.disabled = currentPageNumber === pagingInfo.totalPages;
-    previousButton.disabled = currentPageNumber === 1;
-}
-
-export default function loadPaging(callback) {
-    nextButton.addEventListener('click', () => {
-        callback({
-            page: currentPageNumber + 1
-        });
-    });
-
-    previousButton.addEventListener('click', () => {
-        callback({
-            page: currentPageNumber - 1
-        });
-    });
-}
--- src/search-component.js
+++ src/search-component.js
@@ -1,25 +1,2 @@
 const searchForm = document.getElementById('search-form');
 const searchTermInput = searchForm.querySelector('input');
-
-
-export default function loadSearch(callback) {
-    searchForm.addEventListener('submit', event => {
-        event.preventDefault();
-        
-        const searchTerm = searchTermInput.value;
-
-        if(searchTerm.trim() === '') {
-            return;
-        }
-
-        const searchOptions = {
-            term: searchTerm
-        };
-
-        callback(searchOptions);
-    });
-}
-
-export function updateSearchTerm(searchOptions) {
-    searchTermInput.value = searchOptions.term;
-}
