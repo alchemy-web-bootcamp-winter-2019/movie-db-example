602054d7f30aad5298d989a0756a3ae5f5764422
martypdx <marty@martypdx.com>
2019-03-08 10:05:50 -0800

    update test file name

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
 import './html-equal.js';
 import './movies-component.test.js';
 import './hash-query.test.js';
-import './make-search-movie-url.test.js';
+import './movie-api.test.js';
 import './movie-detail-component.test.js';
