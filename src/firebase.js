2a046647177439edf608e366f32ee251df3f70ab
martypdx <marty@martypdx.com>
2019-03-12 08:09:09 -0700

    setup firebase stuff

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,13 @@
 const config = {
     apiKey: 'AIzaSyC8RBblrwHfd4dt_hfpSCc6ujYj-T_LWe4',
-    authDomain: 'user-demo-c6218.firebaseapp.com'
+    authDomain: 'user-demo-c6218.firebaseapp.com',
+    databaseURL: "https://user-demo-c6218.firebaseio.com",
+    projectId: "user-demo-c6218",
 };
 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
+
+const db = firebase.database();
+export const favoritesByUserRef = db.ref('favorites-by-user');
